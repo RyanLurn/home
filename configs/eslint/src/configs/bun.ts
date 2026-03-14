fed916faa025ad4916e0527c6baad106ab591138
Ryan Luong <lumintriet@gmail.com>
2026-03-14 19:08:53 +0700

    feat(eslint): define bun config.

---
+++ configs/eslint/src/configs/bun.ts
@@ -0,0 +1,19 @@
+import { baseConfig } from "@/configs/base";
+import prettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export const bunConfig = defineConfig([
+  baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+      globals: {
+        ...globals.bunBuiltin,
+      },
+    },
+  },
+  prettier,
+]);
