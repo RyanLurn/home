5351beb18da6ee9def99019da0fcb81c87225372
Ryan Luong <lumintriet@gmail.com>
2026-03-14 19:11:55 +0700

    feat(eslint): define browser config.

---
+++ configs/eslint/src/configs/browser.ts
@@ -0,0 +1,25 @@
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import globals from "globals";
+import { baseConfig } from "@/configs/base";
+import tanstackRouter from "@tanstack/eslint-plugin-router";
+
+export const browserConfig = defineConfig([
+  baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  tanstackRouter.configs["flat/recommended"],
+  reactHooks.configs.flat.recommended,
+  jsxA11y.flatConfigs.recommended,
+  prettier,
+]);
