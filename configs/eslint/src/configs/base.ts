c2ca3878a1a53ea940c282cb4b4661ac904857ea
Ryan Luong <lumintriet@gmail.com>
2026-03-14 19:07:52 +0700

    feat(eslint): define base config.

---
+++ configs/eslint/src/configs/base.ts
@@ -0,0 +1,31 @@
+import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig } from "eslint/config";
+import ts from "typescript-eslint";
+import js from "@eslint/js";
+
+import { ignoreConfig } from "@/ignore";
+
+export const baseConfig = defineConfig([
+  ignoreConfig,
+  js.configs.recommended,
+  ts.configs.recommendedTypeChecked,
+  {
+    rules: {
+      "@typescript-eslint/only-throw-error": [
+        "error",
+        {
+          allow: [
+            {
+              package: "@tanstack/router-core",
+              name: "Redirect",
+              from: "package",
+            },
+          ],
+        },
+      ],
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+  perfectionist.configs["recommended-line-length"],
+]);
