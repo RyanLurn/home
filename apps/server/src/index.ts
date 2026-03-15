007da7d8b36ef0170637b4a75d6165ecc827d18a
Ryan Luong <lumintriet@gmail.com>
2026-03-15 09:44:24 +0700

    feat(server): use the port number constant from core package.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,10 @@
+import { SERVER_PORT } from "@home/core/constants/ports";
 import { Hono } from "hono";
 
 const app = new Hono();
 app.get("/", (c) => c.text("Hello client!"));
 
-export default app;
+export default {
+  port: SERVER_PORT,
+  fetch: app.fetch,
+};
