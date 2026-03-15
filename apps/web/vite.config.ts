735a92a4f34c5baa2f64b5d2e6841ab79e2ec3d7
Ryan Luong <lumintriet@gmail.com>
2026-03-15 10:26:49 +0700

    feat(web): add TanStack Start config.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,4 @@
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { WEB_CLIENT_PORT } from "@home/core/constants/ports";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -7,15 +7,15 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [
-    // Make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-    tanstackRouter({
-      autoCodeSplitting: true,
-      quoteStyle: "double",
-      semicolons: true,
-      target: "react",
+    // Make sure that '@tanstack/react-start/plugin/vite' is passed before '@vitejs/plugin-react'
+    tanstackStart({
+      router: {
+        quoteStyle: "double",
+        semicolons: true,
+      },
     }),
-    tailwindcss(),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
